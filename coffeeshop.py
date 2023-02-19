6006154b2d04ea6b1589b59b3411e52d0f144db2
jungsehee <saehei061025@gmail.com>
2023-02-19 13:04:13 +0900

    coffeeshop

--- coffeeshop.py
+++ coffeeshop.py
@@ -5,6 +5,9 @@ class Coffeeshop:
         self.coffeeList = {"아이스 아메리카노": 2000,
                            "라떼": 3000,
                            "딸기 스무디": 4000}
+        self.stockList = {"아이스 아메리카노": 10,
+                          "라떼": 15,
+                          "딸기 스무디": 20}
     def introducing(self):
         print(f"안녕하세요. {self.name}입니다. 재산은 {self.property}원입니다.")
     def addMenu(self):
@@ -14,3 +17,8 @@ class Coffeeshop:
     def showMenu(self):
         for x, y in self.coffeeList.items():
             print(f"이름: {x} 가격: {y}")
+    def showStock(self):
+        for x, y in self.stockList.items():
+            print(f"이름: {x} 재고: {y}")
+    def sale(self):
+        a = input("구매할 음료의 이름을 입력해주세요: ")
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import school
+import coffeeshop
 
-a = school.Student("kim", 3)
-b = school.Student("pack", 2)
-c = school.Student("jung", 1)
-mathTeacher = school.Teacher("lee", "수학")
-ban1 = school.Class(1, mathTeacher)
-
-ban1.addStudent(a)
-ban1.addStudent(b)
-ban1.addStudent(c)
-ban1.introducing()
-ban1.mathavg()
-a.studyMath()
-a.studyMath()
-ban1.mathavg()
+shop = coffeeshop.Coffeeshop("세희커피")
+shop.introducing()
+shop.showMenu()
+shop.showStock()
